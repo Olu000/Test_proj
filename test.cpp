016f6661ae0ef63ef0590edf884c69b29577d120
Paul Ogunniran <pogunniran5@gmail.com>
2026-05-06 13:59:44 -0500

    commit3

--- test.cpp
+++ test.cpp
@@ -1,5 +1,8 @@
-#include<iostream>
-
+#include <sys/socket.h>// For socket functions
+#include <netinet/in.h>// for sockaddr_in
+#include <cstdlib>// For exit() and EXIT_failure
+#include <iosteream> // For cout
+#include <unistd.h> // For read
 int main(){
     std:: cout << "Hey dude" << std::endl;
     return 0;
