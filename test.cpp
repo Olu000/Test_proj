64a582ee8924b366d1b244b0906d8c93a3803047
Paul Ogunniran <pogunniran5@gmail.com>
2026-05-06 14:13:02 -0500

    commit4

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@ int main(){
    // Create a scoket (IPv4,TCP)
    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
    if(sockfd == -1){
-    std:: cout <<"Failed to create socket. errno:"<< errno << std::endl;
+    std:: cout <<"Failed to creating socket. errno:"<< errno << std::endl;
     exit(EXIT_FAILURE);
    }
     return 0;
